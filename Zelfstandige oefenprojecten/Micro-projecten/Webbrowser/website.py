f48125db8dc93e6110c1b99c7b6911615d921731
Viradj Ramlochan <viradj97@gmail.com>
2017-10-26 13:53:32 +0200

    Smale file update
    First smal file update

---
+++ Zelfstandige oefenprojecten/Micro-projecten/Webbrowser/website.py
@@ -0,0 +1,15 @@
+import webbrowser
+
+# url
+blackboard	= 'https://blackboard.hhs.nl/'
+github		= 'https://github.com/Viradj/Portfolio-ADS'
+nos			= 'https://nos.nl'
+keep		= 'https://keep.google.com'
+
+
+
+# open webbrowser
+webbrowser.open_new_tab(blackboard)
+webbrowser.open_new_tab(github)
+webbrowser.open_new_tab(nos)
+webbrowser.open_new_tab(keep)
